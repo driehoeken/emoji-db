7cbf6ab6b87676ebb0b5d31a8e1af8b8f4f14e58
driehoeken <a.bednarski1203@gmail.com>
2023-06-15 15:02:47 +0200

    displaying categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import Category from "./components/Category";
 import Header from "./components/Header";
 
 function App() {
+    const categories = [
+        "smileys-and-people",
+        "animals-and-nature",
+        "food-and-drink",
+        "travel-and-places",
+        "activities",
+        "objects",
+        "symbols",
+        "flags",
+    ];
     return (
         <>
             <Header />
-            <main></main>
+            <main>
+                {categories.map((category) => {
+                    return <Category category={category} />;
+                })}
+            </main>
         </>
     );
 }
